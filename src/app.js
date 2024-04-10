45813731de41533e3e1ee1bedc3c1681026e3443
Mauros721 <157764840+Mauros721@users.noreply.github.com>
2024-04-10 14:30:05 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const cards = ['♥', '♣', '♦', '♠'];
+const numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']; 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function getCard() {
+  const singleCard = cards[Math.floor(Math.random() * cards.length)];
+  const singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  const showCard = `${singleNumber}   ${singleCard}`;
+  document.querySelector( '#showing' ).innerHTML = showCard;
+}
